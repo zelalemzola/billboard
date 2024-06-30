443c594b46b2e9cca056a6c2d6faf1e76b824ab8
zelalemzola <tzelalemtesfaye@gmail.com>
2024-06-30 20:57:47 +0300

    landing page v1

--- src/components/HeroBottom.jsx
+++ src/components/HeroBottom.jsx
@@ -7,12 +7,12 @@ export function HeroBottom() {
   return (
     <div className="h-[40rem] w-full bg-white flex flex-col items-center justify-center overflow-hidden rounded-md">
       <div className="flex flex-col gap-5 pb-10">
-      <h1 className="md:text-3xl text-2xl lg:text-4xl font-bold text-center text-black relative z-20 pt-20">
+      <h1 className="md:text-3xl text-2xl lg:text-4xl font-bold text-center text-black relative z-20 pt-40">
        Connect with us, Increase your Prescence !
       </h1>
       <Link href='/' className="rounded-full bg-black text-white px-6 py-5 w-fit mx-auto">Contact Us</Link>
         </div>
-      <div className="w-[40rem] h-40 relative">
+      <div className="w-[40rem] h-40 relative rounded-b-full">
         {/* Gradients */}
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[3px] w-3/4 blur-sm" />
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[3px] w-3/4" />
